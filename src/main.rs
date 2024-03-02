60f20a2a2bec8ff88faf598fc98de598a9ee6cec
L3Sota <l3sota@pm.me>
2024-03-02 19:00:57 +0900

    feat(main): parse file into string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::Read};
+
 use clap::Parser;
 
 /// Simple program to parse output from `chia wallet get_transactions`
@@ -12,5 +14,15 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    eprintln!("Parsing {}", args.file_path)
+    eprintln!("Parsing {}", args.file_path);
+
+    let mut file = File::open(args.file_path).expect("failed to open file");
+    let mut content = {
+        let cap = file.metadata().expect("failed to obtain file metadata").len();
+        String::with_capacity(cap.try_into().unwrap_or(0))
+    };
+    file.read_to_string(&mut content).expect("failed to read file to string");
+
+    // TODO
+    println!("{}", content)
 }
